f064ca9db91d8d0e39ac84802acdc179789581a7
pedrovgas <52414353+pedrovgas@users.noreply.github.com>
2020-05-26 08:25:54 -0300

    Updates

---
+++ precache-manifest.5bdfc9936fff919f6a1c4ad582cb9c75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4023a74aeb7230b8b5028bf9143dba8e",
+    "url": "/pedrovgas.github.io/index.html"
+  },
+  {
+    "revision": "b3d470db6c9db6a94ccc",
+    "url": "/pedrovgas.github.io/static/js/2.da2cb003.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pedrovgas.github.io/static/js/2.da2cb003.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9affd390c27147f3fb4",
+    "url": "/pedrovgas.github.io/static/js/main.22f4a771.chunk.js"
+  },
+  {
+    "revision": "ee44ea4a74b6d544bd07",
+    "url": "/pedrovgas.github.io/static/js/runtime-main.777a0d1c.js"
+  },
+  {
+    "revision": "cf96f12a46b604df3a53ee0cdc4a68da",
+    "url": "/pedrovgas.github.io/static/media/logo.cf96f12a.svg"
+  }
+]);
